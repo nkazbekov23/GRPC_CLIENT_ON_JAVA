8c41fcda1755e70f2df1c5866aedbe21c2175064
nursultankazbekov <refresh1101>
2021-02-26 10:18:36 +0600

    init commit

---
+++ src/main/java/org/example/grpc/App.java
@@ -0,0 +1,32 @@
+package org.example.grpc;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.GreetingServiceOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forTarget("localhost:8081")
+                .usePlaintext()
+                .build();
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub =
+                GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
+                .newBuilder().setName("Nursultan")
+                .build();
+
+        GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
+        System.out.println(response);
+        channel.shutdownNow();
+
+    }
+}
